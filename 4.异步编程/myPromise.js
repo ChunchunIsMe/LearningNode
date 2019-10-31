45ddf8cbbb7408755ed1107926e16105bc1fdf02
linlichun <linlichun@meicai.cn>
2019-10-31 18:33:37 +0800

    finish promise

--- 4.异步编程/myPromise.js
+++ 4.异步编程/myPromise.js
@@ -1,84 +1,115 @@
-const PENDING = 'PENDING';
-const RESOLVED = 'RESOLVED';
-const REJECTED = 'REJECTED';
+var PENDING = 'PENDING';
+var RESOLVED = 'RESOLVED';
+var REJECTED = 'REJECTED';
+
 
 function MyPromise(fn) {
-  if (typeof fn !== 'function') {
-    console.log('not function');
-  }
   this.value = undefined;
   this.status = PENDING;
-  this.resolvedQueue = [];
-  this.rejectedQueue = [];
+  this.resolveQueue = [];
+  this.rejectQueue = [];
+  if (typeof fn !== 'function') {
+    throw new Error('not function');
+  }
   fn(resolve.bind(this), reject.bind(this));
 }
 
+var resolve = function (value) {
+  if (this.status !== PENDING) {
+    return;
+  }
+  this.value = value;
+  this.status = RESOLVED;
+  var that = this;
+  var cb;
+  if (value instanceof MyPromise) {
+    value.then(function (value) {
+      while (cb = that.resolveQueue.shift()) {
+        cb(value);
+      }
+    }, function (value) {
+      while (cb = that.rejectQueue.shift()) {
+        cb(value);
+      }
+    })
+  } else {
+    while (cb = this.resolveQueue.shift()) {
+      cb(value);
+    }
+  }
+}
+
+var reject = function (value) {
+  if (this.status !== PENDING) {
+    return;
+  }
+  this.value = value;
+  this.status = REJECTED;
+  var cb;
+  while (cb = this.rejectQueue.shift()) {
+    cb(value);
+  }
+}
+
+
 MyPromise.prototype.then = function (handleResolve, handleReject) {
-  var that = this
-  // console.log(this.value, 111);
-  // console.log(this.status, 222);
+  var that = this;
 
   return new MyPromise(function (nextResolve, nextReject) {
+
+    var toNextResolve = function (value) {
+      if (typeof nextResolve !== 'function') {
+        throw new Error('not function');
+      }
+      var result = handleResolve(value);
+      if (result instanceof MyPromise) {
+        result.then(nextResolve, nextReject);
+      } else {
+        nextResolve(result);
+      }
+    }
+
+    var toNextReject = function (value) {
+      if (typeof nextReject !== 'function') {
+        throw new Error('not function');
+      }
+      var result = handleReject(value);
+      if (result instanceof MyPromise) {
+        result.then(nextResolve, nextReject);
+      } else {
+        nextReject(result);
+      }
+    }
+
     switch (that.status) {
       case PENDING:
-        this.resolvedQueue.push(handleResolve);
-        this.rejectedQueue.push(handleReject);
+        that.resolveQueue.push(toNextResolve);
+        that.rejectQueue.push(toNextReject);
         break;
       case RESOLVED:
-        var result = handleResolve(that.value);
-        nextResolve(result);
+        toNextResolve(that.value);
         break;
       case REJECTED:
-        nextReject(handleReject(that.value))
+        toNextReject(that.value);
         break;
     }
   })
 }
 
-var resolve = function (data) {
-  if (this.status !== PENDING) {
-    return;
-  }
-  this.status = RESOLVED;
-  this.value = data;
-  var cb;
-  while (cb = this.resolvedQueue.shift()) {
-    cb(data);
-  }
-}
-var reject = function (err) {
-  if (this.status !== PENDING) {
-    return;
-  }
-  this.status = REJECTED;
-  this.value = err;
-  var cb;
-  while (cb = this.rejectedQueue.shift()) {
-    cb(err)
-  }
-}
+var a = new MyPromise(function (re, rj) {
+  setTimeout(function () {
+    re(1);
+  }, 1000)
+})
 
-
-var a = new MyPromise(function (re) {
-  re(1)
-});
-
-setTimeout(function () {
-  a.then(function (data) {
-    data += 1;
-    console.log(data);
-    return data;
-  }).then(function (data) {
-    data += 1;
-    console.log(data);
-    return data;
-  }).then(function (data) {
-    data += 1;
-    console.log(data);
-    return data;
-  }).then(function (data) {
-    data += 1;
-    console.log(data);
-    return data;
+a.then(function () {
+  return new MyPromise(function (re) {
+    re(new MyPromise(function (re, rj) {
+      rj(123);
+    }))
   })
-}, 1000)
+}).then(function () {
+  console.log('resolve');
+}, function () {
+  console.log('reject');
+})
