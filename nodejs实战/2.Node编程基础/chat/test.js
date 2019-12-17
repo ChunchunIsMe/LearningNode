d39883ec21e73aaf07506a1dcd83efffb4fe3d47
linlichun <linlichun@meicai.cn>
2019-12-17 11:13:27 +0800

    finish 3

--- nodejs实战/2.Node编程基础/chat/test.js
+++
@@ -1,8 +0,0 @@
-
-var http = require("http")
-http.createServer(function (req, res) {//回调函数
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write("holloe  world")
-  console.log(req.url);
-  res.end("fdsa");
-}).listen(3000);
